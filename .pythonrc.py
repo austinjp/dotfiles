d55028e9dcbc6a2be17255d0c6ea7646696e9ed6
austinjp <austin.plunkett@gmail.com>
2022-02-08 14:54:44 +0000

    Fixing some logging issues in ptpython

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,6 +1,9 @@
-import sys
+import os
 
 from pathlib import Path
+
+if not "PTPYTHON_CONFIG_HOME" in os.environ:
+    os.environ["PTPYTHON_CONFIG_HOME"] = str(Path.home() / ".config/ptpython")
 
 
 try:
@@ -8,10 +11,23 @@ try:
 except ImportError:
     print("ptpython is not available: falling back to standard prompt")
 else:
+    import logging, sys
+
+    logger = logging.getLogger()
+    logger.setLevel(10)
+
+    # The following prevent ptpython's buggy (?) logging filling the repl :)
+    parso_logger = logging.getLogger("parso.python.diff")
+    parso_logger.setLevel(100)
+    asyncio_logger = logging.getLogger("asyncio")
+    asyncio_logger.setLevel(100)
+
     sys.exit(
         embed(
-            history_filename=str(Path.home()) + "/.local/share/ptpython/history",
+            history_filename=str(Path.home() / ".local/share/ptpython/history"),
             globals=globals(),
-            locals=locals()
+            locals=locals(),
+            patch_stdout=True,
+            title="repl"
         )
     )
