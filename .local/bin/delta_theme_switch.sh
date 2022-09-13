450eea538fd0c0299cb2471a53f3dce256ad346e
austinjp <austin.plunkett@gmail.com>
2022-09-13 14:28:24 +0000

    A bunch of util scripts

---
+++ .local/bin/delta_theme_switch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if is_terminal_dark.sh ; then
+    echo "--dark"
+else
+    echo "--light"
+fi
---
+++ .local/bin/is_terminal_dark.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -f ~/.config/xfce4/terminal/terminalrc ] ; then
+    export bg_hex=$(ag ColorBackground ~/.config/xfce4/terminal/terminalrc | sed -e 's/^.*\=\#//')
+    export r_hex=$(echo $bg_hex | head -c 2 | tail -c 2)
+    export g_hex=$(echo $bg_hex | head -c 4 | tail -c 2)
+    export b_hex=$(echo $bg_hex | head -c 6 | tail -c 2)
+    if [[ "0x${r_hex}" -lt "0xcc" ]] && [[ "0x${g_hex}" -lt "0xcc" ]] && [[ "0x${b_hex}" -lt "0xcc" ]] ; then
+        exit 0 # true
+    else
+        if [[ "0x${r_hex}" -lt "0xcc" ]] && [[ "0x${g_hex}" -lt "0xcc" ]] ; then
+            exit 0 # true
+        elif [[ "0x${r_hex}" -lt "0xcc" ]] && [[ "0x${b_hex}" -lt "0xcc" ]] ; then
+            exit 0 # true
+        elif [[ "0x${g_hex}" -lt "0xcc" ]] && [[ "0x${b_hex}" -lt "0xcc" ]] ; then
+            exit 0 #true
+        fi
+    fi
+fi
+
+exit 1 # false
