4594624a80f760d5b6e6b99367a35fb3052374ca
austinjp <austin.plunkett@gmail.com>
2022-09-15 12:48:55 +0000

    Assume terminal is dark if background cannot be determined

--- .local/bin/is_terminal_dark.sh
+++ .local/bin/is_terminal_dark.sh
@@ -2,6 +2,10 @@
 
 if [ -f ~/.config/xfce4/terminal/terminalrc ] ; then
     export bg_hex=$(ag ColorBackground ~/.config/xfce4/terminal/terminalrc | sed -e 's/^.*\=\#//')
+    if [ "x${bg_hex}" == "x" ]; then
+        # If no background colour is specified, assume it's black.
+        export bg_hex=000000
+    fi
     export r_hex=$(echo $bg_hex | head -c 2 | tail -c 2)
     export g_hex=$(echo $bg_hex | head -c 4 | tail -c 2)
     export b_hex=$(echo $bg_hex | head -c 6 | tail -c 2)
