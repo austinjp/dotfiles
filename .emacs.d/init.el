516618c59fe4920c1075225b8f93b232bf545211
austinjp <austin.plunkett@gmail.com>
2023-11-05 09:28:01 +0000

    Cython mode, and associated file-name extensions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -63,7 +63,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(eldoc multiple-cursors basic-mode fold-this haxe-mode lua-mode magit js2-mode flymake-css yaml-mode undo-tree rainbow-delimiters eglot sideline-flymake sideline markdown-mode multi-web-mode json-mode company cmake-mode)))
+   '(cython-mode eldoc multiple-cursors basic-mode fold-this haxe-mode lua-mode magit js2-mode flymake-css yaml-mode undo-tree rainbow-delimiters eglot sideline-flymake sideline markdown-mode multi-web-mode json-mode company cmake-mode)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -118,11 +118,16 @@
   :config
   (setq use-package-always-ensure 't))
 
-;; Manually kick-start lorem-ipsum.el
+;; Kick-start a bunch of packages:
+;; lorem-ipsum.el
 (require 'lorem-ipsum)
-
-;; Manually kick-start quarto-mode for Quarto files.
+;; Quarto-mode for Quarto files.
 ;; (require 'quarto-mode)
+(add-to-list 'auto-mode-alist '("\\.qmd" . quarto-mode))
+;; Cython mode.
+(add-to-list 'auto-mode-alist '("\\.pyx" . cython-mode))
+(add-to-list 'auto-mode-alist '("\\.pxd" . cython-mode))
+;; (require 'cython-mode)
 
 ;; ======================================================================
 
@@ -294,10 +299,16 @@
       kept-new-versions 6
       kept-old-versions 2
       version-control t)       ; use versioned backups
+
+;; (setq backup-directory-alist
+;;       `((".*" . ,temporary-file-directory)))
+;; (setq auto-save-file-name-transforms
+;;       `((".*" ,temporary-file-directory t)))
+
 (setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
+       '(("." . "~/.local/tmp/")))
 (setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+       `((".*" ,"~/.local/tmp/" t)))
 
 ;; Indentation.
 (setq-default indent-tabs-mode nil)
