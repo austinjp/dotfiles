53912dc03d8405611e7c836b6d17a411eca6d439
austinjp <austin.plunkett@gmail.com>
2025-04-16 13:04:52 +0100

    Tweak font colours

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,7 +75,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-builtin-face ((((type tty)) (:foreground "cyan"))))
+ '(font-lock-builtin-face ((((type tty)) (:foreground "royalblue3"))))
+ '(font-lock-keyword-face ((((type tty)) (:foreground "green4"))))
  '(font-lock-function-name-face ((t (:foreground "color-33"))))
  '(font-lock-string-face ((((type tty)) (:foreground "purple"))))
  '(jinx-highlight ((t (:inherit isearch :background "firebrick2"))))
